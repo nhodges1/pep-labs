742e33d0444fe7d88ff3bdf56d635756b03a1bcd
nhodges1 <hodges27@outlook.com>
2022-12-20 21:28:40 +0000

    Saved progress on HelloWorld 2022-12-20 21:28:40 +0000

--- HelloWorld/src/main/java/HelloWorld.java
+++ HelloWorld/src/main/java/HelloWorld.java
@@ -6,7 +6,12 @@ public class HelloWorld {
      * You can use the test results to check for differences.
      * @return "Hello, world!"
      */
-    public String sayHello(){
-        return "change me";
+    public static void main(String[] args) {
+        public String sayHello() {
+            return "Hello, world";
+        }
+    }
+    public String sayHello() {
+        return "Hello, world";
     }
 }
