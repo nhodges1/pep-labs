a18448bcde9d024e217c138b59145869fca0ef45
nhodges1 <hodges27@outlook.com>
2022-12-21 04:00:10 +0000

    Saved progress on CombineStrings 2022-12-21 04:00:10 +0000

--- CombineStrings/src/main/java/CombineStrings.java
+++ CombineStrings/src/main/java/CombineStrings.java
@@ -7,7 +7,7 @@ public class CombineStrings {
      * @param b the second part of the string to be combined.
      * @return the combination of strings a and b.
      */
-    public String combine(String a, String b){
-        return "a + b";
+    public String combine(String input1, String input2){
+        return input1 + input2;
     }
 }
