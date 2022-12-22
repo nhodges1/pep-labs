47d20909a142da1fb757b296e301f07c4d5aeda6
nhodges1 <hodges27@outlook.com>
2022-12-22 03:05:37 +0000

    Saved progress on Instantiation 2022-12-22 03:05:37 +0000

--- Instantiation/src/main/java/InstantiationExample.java
+++ Instantiation/src/main/java/InstantiationExample.java
@@ -18,6 +18,7 @@ public class InstantiationExample {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        Object Training = new Object();
+        return Training;
     }
 }
