790a165db25bf17d5efdd288fccf9e958837b473
nhodges1 <hodges27@outlook.com>
2023-01-04 00:38:35 +0000

    Saved progress on DoesNotContain 2023-01-04 00:38:35 +0000

--- DoesNotContain/src/main/java/DoesNotContain.java
+++ DoesNotContain/src/main/java/DoesNotContain.java
@@ -15,9 +15,9 @@ public class DoesNotContain {
     public boolean arrayDoesNotContain(int[] arr, int target){
         
         for (int i = 0 ; i < arr.length; i++){
-            if (arr[i] != target){
-                return true;
+            if (arr[i] == target){
+                return !true;
             } 
-        } return false;
+        } return !false;
     }
 }
