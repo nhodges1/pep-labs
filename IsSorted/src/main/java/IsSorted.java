2256a3106c23e78fcba543bf08dd0d6be5560b75
nhodges1 <hodges27@outlook.com>
2023-01-11 07:14:33 +0000

    Saved progress on IsSorted 2023-01-11 07:14:33 +0000

--- IsSorted/src/main/java/IsSorted.java
+++ IsSorted/src/main/java/IsSorted.java
@@ -8,9 +8,9 @@ public class IsSorted {
      */
     public boolean checkSort(int[] arr){
         
-    for (int i = 0; i < arr.length; i++)
-        if (arr[i] < arr[i + 1]) {
-            return false;
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1])
+                return false;
         }
         return true;
     }
