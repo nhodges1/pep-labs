d29e710c953caf4dd78fb2483648a4b78f0d3b39
nhodges1 <hodges27@outlook.com>
2023-01-19 18:24:17 +0000

    Saved progress on InnerJoin 2023-01-19 18:24:17 +0000

--- InnerJoin/problem1.sql
+++ InnerJoin/problem1.sql
@@ -1,4 +1,5 @@
-SELECT student.id, student.student_name
-FROM student
-INNER JOIN class ON student.class_title = class.class_title
+SELECT student.id, student.student_name 
+FROM student 
+INNER JOIN class 
+ON student.class_title = class.class_title 
 WHERE class.teacher_name = 'Ms. Lovelace';
