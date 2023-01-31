df61f490658c53ad12b7c3108cc6b7e7c092a11b
nhodges1 <hodges27@outlook.com>
2023-01-31 17:42:27 +0000

    Saved progress on Library 2023-01-31 17:42:27 +0000

--- Library/src/main/java/Application/DAO/BookDAO.java
+++ Library/src/main/java/Application/DAO/BookDAO.java
@@ -114,11 +114,11 @@ public class BookDAO {
         List<Book> books = new ArrayList<>();
         try {
             //Write SQL logic here
-            String sql = "SELECT * FROM Book count(?) > 0";
+            String sql = "SELECT * FROM Book WHERE copies_available > 0";
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
             //write preparedStatement's setInt method here.
-            preparedStatement.setInt(1, book.getBooksWithBookCountOverZero());
+            // preparedStatement.setInt(1, book.getBooksWithBookCountOverZero());
 
             ResultSet rs = preparedStatement.executeQuery();
             while(rs.next()){
