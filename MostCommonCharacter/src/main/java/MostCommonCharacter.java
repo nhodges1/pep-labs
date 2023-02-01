f5a6bb3a58f39e088e2909025ea267900bcdec73
nhodges1 <hodges27@outlook.com>
2023-02-01 06:49:02 +0000

    Saved progress on Fibonacci 2023-02-01 06:49:02 +0000

--- MostCommonCharacter/src/main/java/MostCommonCharacter.java
+++ MostCommonCharacter/src/main/java/MostCommonCharacter.java
@@ -7,7 +7,28 @@ public class MostCommonCharacter {
      * @param str A String.
      * @return the most common character within str.
      */
-    public char recurringChar(String str) {
-        return ' ';
+    static final int ASCII_SIZE = 256;
+    public char recurringChar(String str) {    
+        int count[] = new int[ASCII_SIZE];
+ 
+        // Construct character count array from the input
+        // string.
+        int len = str.length();
+        for (int i = 0; i < len; i++)
+            count[str.charAt(i)]++;
+ 
+        int max = -1; // Initialize max count
+        char result = ' '; // Initialize result
+ 
+        // Traversing through the string and maintaining
+        // the count of each character
+        for (int i = 0; i < len; i++) {
+            if (max < count[str.charAt(i)]) {
+                max = count[str.charAt(i)];
+                result = str.charAt(i);
+            }
+        }
+ 
+        return result;
     }
 }
